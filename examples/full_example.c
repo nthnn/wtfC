84334dda40c2d58efd5cd06dbc1e3f5abc262e05
nthnn <nathanneisip@gmail.com>
2023-11-26 03:26:10 +0800

    Full wtfC example.

---
+++ examples/full_example.c
@@ -0,0 +1,14 @@
+import <stdio.h>
+
+main(i32 argc, rune* argv[])
+begin
+	nothing;
+
+	i32* test = make(i32, 10);
+	test[9] = 100;
+
+	echo(test[9]);
+	echo("\nHello, damn world!\r\n");
+
+	for ever;
+end
